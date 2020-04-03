6269c59cd5c7711c4da438e9c7057da9a5e3ccab
sqiyan <60425964+sqiyan@users.noreply.github.com>
2020-04-04 00:10:51 +0800

    Add files via upload
    .py code for livestreaming widget on kivy from RPi Camera

---
+++ livestreamkivy.py
@@ -0,0 +1,26 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.video import Video
+from kivy.lang import Builder
+from kivy.uix.videoplayer import VideoPlayer
+kivy.require("1.11.1")
+
+# class EpicApp(App):
+#     def build(self):
+#         return Label(text = "hey there!")
+    
+# if __name__ == "__main__":
+#     EpicApp().run() 
+
+root = Builder.load_string('''
+VideoPlayer:
+    source: 'http://10.21.143.20:8081/stream.mjpg'
+''')
+
+class TestApp(App):
+	def build(self):
+		return root
+
+if __name__ == '__main__':
+    TestApp().run()
