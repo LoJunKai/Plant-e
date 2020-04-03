df759966211b975b800cf61adbc33ba8ad073afc
LoJunKai <44375442+LoJunKai@users.noreply.github.com>
2020-04-03 22:25:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import smbus
 import base64
 from picamera import PiCamera
 import time
-import database
+import database  # Download the file database.py
 
 #bus = smbus.SMBus(0) # Rev 1 Pi uses 0
 bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
