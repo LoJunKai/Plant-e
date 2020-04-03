8d940b80573417beeac5bd1f8ffc0d9bbbce366c
LoJunKai <44375442+LoJunKai@users.noreply.github.com>
2020-04-04 02:42:13 +0800

    Update Raspi_Setup.sh

--- Raspi_Setup.sh
+++ Raspi_Setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# INSTRUCTIONS TO RUN FILE
+# cd into Desktop
+# sudo bash Raspi_Setup.sh
+
 sudo raspi-config nonint do_i2c 0     # Enable i2c
 sudo raspi-config nonint do_camera 0  # Enable Camera
 reboot
