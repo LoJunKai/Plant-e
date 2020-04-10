297794ea3737ce99c93472f899bb2bda4e4fe102
LoJunKai <44375442+LoJunKai@users.noreply.github.com>
2020-04-11 01:25:08 +0800

    Create camera.py

---
+++ camera.py
@@ -0,0 +1,12 @@
+from picamera import PiCamera
+
+def takepic(day):
+    with PiCamera() as camera:
+        camera.start_preview()
+        camera.capture('/home/pi/Desktop/day{}.jpg'.format(day))
+        camera.stop_preview()
+    with open("/home/pi/Desktop/day{}.jpg".format(day), "rb") as imageFile:
+        imgbytes = base64.b64encode(imageFile.read())
+        imgstr = imgbytes.decode('utf-8')
+        db.child("Camera").child("day " + str(day)).set(imgstr, user['idToken'])
+    return
