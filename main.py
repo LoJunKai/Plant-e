3f2a6f6f736af97ab3303aef9e40d9f1e70dbf42
LoJunKai <44375442+LoJunKai@users.noreply.github.com>
2020-04-11 03:35:26 +0800

    Added comments and passed pep8

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
 # Make sure you have run pip install -r requirements.txt to download the necessary dependencies
-from database import *
-from sensors import *
-from ave3 import cal_ave3
-from camera import takepic
-import smbus
-
-# Change accordingly if SMBus error occurs
-# bus = smbus.SMBus(0) # Rev 1 Pi uses 0
-bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
-
-user, db = db_setup("plant-e")
-
 
 """
 Crontab 101:
@@ -21,33 +9,58 @@ replacing your command with      sudo reboot now         #this makes your rpi re
 crontab -l   <-- not sure if 1 or l                      #to view file without editing
 """
 
-# Get I2C address using: sudo i2cdetect -y 1
-LDR = (0x23, 0x5c) # LDR[0] = Default device I2C address, LDR[1] = set as per instructions below
+from database import *
+from sensors import *
+from ave3 import cal_ave3
+from camera import takepic
+import smbus
 
+# Change accordingly if SMBus error occurs
+# bus = smbus.SMBus(0) # Rev 1 Pi uses 0
+bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
+
+# Setup database
+user, db = db_setup("plant-e")
+
+# Get I2C address using: sudo i2cdetect -y 1
+LDR = (0x23, 0x5c)  # LDR[0] = Default device I2C address, LDR[1] = set as per instructions below
+
+
+# Get data from the sensors
 def get_data(plant):
     """ return {"light" : 123, "moisture" : 321} picture is in rpi camera code """
-    
-    index = {100:0, 101:1, 102:2, 104:3, 110:4, 111:5}
-    plant = index[plant]
-    
-    if plant == 0 or plant == 1 or plant == 2:
-    	ldr = LDR[0]
-    else:
-    	ldr = LDR[1]
 
-    data = {"light" : read_light(bus, ldr), "moisture" : read_moisture_arduino(plant)}
+    index = {100: 0, 101: 1, 102: 2, 104: 3, 110: 4, 111: 5}
+    plant = index[plant]
+
+    if plant == 0 or plant == 1 or plant == 2:
+        ldr = LDR[0]
+    else:
+        ldr = LDR[1]
+
+    data = {"light": read_light(bus, ldr), "moisture": read_moisture_arduino(plant)}
     return data
 
 
-allplantls = get_all_plant_ls(db, user)
-for plant in allplantls:
-    if plant == None:
-        continue
-    plant = int(plant)
-    day = get_day(db, user, plant)
-    hourcount = get_hour(db, user, plant)
-    data = get_data(plant)
-    send_data(db, user, data, plant, day, hourcount)
-    if hourcount == 1:
-        take_pic(db, user, day)
-        cal_ave3(db, user, plant, "day " + str(day))
+if __name__ == '__main__':
+    # Get all plants and iterate through each plant to update all plants
+    allplantls = get_all_plant_ls(db, user)
+    for plant in allplantls:
+
+        # Sometimes the database returns None, so just skip those values
+        if plant is None:
+            continue
+        plant = int(plant)
+
+        # Get time
+        day = get_day(db, user, plant)
+        hourcount = get_hour(db, user, plant)
+
+        # Get sensor data and send it to database
+        data = get_data(plant)
+        send_data(db, user, data, plant, day, hourcount)
+
+        if hourcount == 1:
+            # Code in here is scheduled to be run daily at <hourcount>
+            take_pic(db, user, day)
+            cal_ave3(db, user, plant, "day " + str(day))
